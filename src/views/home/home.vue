0dfe23135fb5e468a2113ab309ef0b4e20f1f71a
Leo_CHEN <chenjing194@gmail.com>
2020-06-01 11:54:00 +0800

    first git

---
+++ src/views/home/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id=''>
+    <h2>首页</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '',
+    data() {
+      return {}
+    },
+    components: {},
+    created() {},
+    mounted() {},
+    methods: {}
+  }
+</script>
+<style scoped>
+</style>
---
+++ src/views/profile/profile.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>我的</h2>
+</template>
+
+<script>
+  export default {
+    name: '',
+    data() {
+      return {}
+    },
+    components: {},
+    created() {},
+    mounted() {},
+    methods: {}
+  }
+</script>
+<style scoped>
+</style>
